23a87eb9f7b1886cf389896c598b49b8cfd4c063
DESKTOP-V6PF4S7\User <wklee@dlit.co.kr>
2024-02-19 14:59:55 +0900

    f

---
+++ MWorkDaily/MWorkDaily/MWorkDaily/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace MWorkDaily.ViewModel;
+
+public class MainViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public MainViewModel() { }
+
+    protected void RaisePropertyChagned(string name)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
